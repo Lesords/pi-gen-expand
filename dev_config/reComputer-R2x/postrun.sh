a6072fb12e505c96fd706966b407a63efc987751
Lesords <lesords168@gmail.com>
2025-06-25 12:22:57 +0000

    feat: add reComputer R2x config file

---
+++ dev_config/reComputer-R2x/postrun.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -e
+set -x
+
+# install examples
+echo ${FIRST_USER_NAME}
+sudo echo ${FIRST_USER_NAME}
+
+cd /home/${FIRST_USER_NAME}
+pwd
+uname -a
+git clone https://github.com/hailo-ai/hailo-rpi5-examples.git
+cd hailo-rpi5-examples
+./install.sh || true
