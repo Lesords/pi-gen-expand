84e4ecd8ce5e20d436fb7db7892f87a3bbbccc6a
Lesords <lesords168@gmail.com>
2025-06-26 11:57:13 +0000

    fix: fix hailo installation issue

--- dev_config/reComputer-R2x/postrun.sh
+++ dev_config/reComputer-R2x/postrun.sh
@@ -3,11 +3,11 @@ set -x
 
 # install examples
 echo ${FIRST_USER_NAME}
-sudo echo ${FIRST_USER_NAME}
 
 cd /home/${FIRST_USER_NAME}
 pwd
 uname -a
-git clone https://github.com/hailo-ai/hailo-rpi5-examples.git
+git clone https://github.com/hailo-ai/hailo-rpi5-examples.git --depth 1
 cd hailo-rpi5-examples
+sed -i 's/device_arch=.*$/device_arch=HAILO8/g' setup_env.sh
 ./install.sh || true
