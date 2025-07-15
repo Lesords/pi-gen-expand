a8f2e899a0ffd481c31ced71fe676a81999b1139
Lesords <lesords168@gmail.com>
2025-07-15 21:49:33 +0800

    fix: solve the problem that the firmware cannot be compiled normally in action

--- dev_config/reComputer-R2x/postrun.sh
+++ dev_config/reComputer-R2x/postrun.sh
@@ -11,3 +11,7 @@ git clone https://github.com/hailo-ai/hailo-rpi5-examples.git --depth 1
 cd hailo-rpi5-examples
 sed -i 's/device_arch=.*$/device_arch=HAILO8/g' setup_env.sh
 ./install.sh || true
+
+free -h
+swapon --show
+df -h
