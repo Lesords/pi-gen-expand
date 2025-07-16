4344ee20e9f8f1e9e0a199fb1b7d2b79c0423edb
Lesords <lesords168@gmail.com>
2025-07-16 11:45:56 +0000

    fix: fix the issue where the systemd service did not overlay the device tree properly

--- dev_config/reComputer-R2x/files/r21_board.sh
+++ dev_config/reComputer-R2x/files/r21_board.sh
@@ -13,6 +13,6 @@ value=$(cat /sys/class/gpio/gpio$DETECT_PIN/value)
 echo $value > /home/pi/result
 
 if [ "$value" == "1" ]; then
-    echo "start to dtoverlay R21-overlay"
-    # dtoverlay reComputer-R21-overlay
+    echo "start to dtoverlay reComputer-R21"
+    dtoverlay reComputer-R21
 fi
