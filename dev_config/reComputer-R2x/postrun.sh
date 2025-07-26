828896cfaf7441f875fc3dcb6b05f9e0e1041b5f
Lesords <lesords168@gmail.com>
2025-07-26 16:02:31 +0800

    chore: remove some package

--- dev_config/reComputer-R2x/postrun.sh
+++ dev_config/reComputer-R2x/postrun.sh
@@ -8,9 +8,6 @@ cd /home/${FIRST_USER_NAME}
 pwd
 uname -a
 git clone https://github.com/hailo-ai/hailo-rpi5-examples.git --depth 1
-cd hailo-rpi5-examples
-sed -i 's/device_arch=.*$/device_arch=HAILO8/g' setup_env.sh
-./install.sh || true
 
 free -h
 swapon --show
